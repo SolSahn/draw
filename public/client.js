573b5a079d022b6a53c1f4e3cc67e55460b3b80c
solsahn@gmail.com <solsahn@gmail.com>
2019-08-16 00:52:55 -0400

    changed from ellipses to lines for smooth drawing

--- public/client.js
+++ public/client.js
@@ -3,24 +3,26 @@ function setup() {
 	// Set up canvas and brush
 	createCanvas(640, 480);
 	background(51);
-	fill(255);
-	noStroke();
+	stroke(255);
+	strokeWeight(3);
 
 	// Connect to server and start receiving and drawing other mouse data
 	socket = io.connect('http://localhost:3000');
 	socket.on('mouse', function(data) {
-		ellipse(data.x, data.y, 12);
+		line(data.x1, data.y1, data.x2, data.y2);
 	});
 }
 
 function mouseDragged() {
-	// Draw ellipse at mouse location
-	ellipse(mouseX, mouseY, 12);
+	// Draw line at from mouse position to previous mouse position
+	line(mouseX, mouseY, pmouseX, pmouseY);
 
 	// Send mouse data to server
 	var data = {
-		x: mouseX,
-		y: mouseY
+		x1: mouseX,
+		y1: mouseY,
+		x2: pmouseX,
+		y2: pmouseY
 	}
 	socket.emit('mouse', data);
 }
